1b5274bae2bc49e2da6893b9a3f1a2283f598287
vvedenin <vvedenin@luxoft.com>
2016-02-09 19:23:04 +0100

    [JLL-1] Add first classes with PastTense

---
+++ polish/src/main/java/com/github/vedenin/polish/PastTenseSentenceBuilding.java
@@ -0,0 +1,10 @@
+package com.github.vedenin.polish;
+
+/**
+ * Created by vvedenin on 2/9/2016.
+ */
+public class PastTenseSentenceBuilding {
+    public static void main(String[] args) {
+        // init version
+    }
+}
